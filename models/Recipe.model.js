0308bee3d5d8c3555e090c3a7facbd2778c70bb0
Louisiane_Berlin <corentine.piette@gmail.com>
2021-02-05 13:55:41 +0100

    stand noon

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -1,10 +1,85 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+
+
 const recipeSchema = new Schema({
-  // TODO: write the schema
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    level: {
+        type: String,
+        enum: ['Easy Peasy','Amateur Chef','UltraPro Chef']
+    },
+    ingredients: {
+        type: [String]
+    },
+    cuisine: {
+        type: String,
+        required: true
+    },
+    dishType: {
+        type: String,
+        enum: ['breakfast', 'main_course', 'soup', 'snack', 'drink', 'dessert', 'other']
+    },
+    image: {
+        type: String,
+        default: "https://images.media-allrecipes.com/images/75131.jpg"
+    },
+    duration: {
+        type: Number,
+        minimum : 0
+    },
+    creator: {
+        type: String,
+    },
+    created: {
+        type: Date,
+        default : Date.now
+    },
+  
+
 });
+
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
 
 module.exports = Recipe;
+
+
+mongoose.connect('mongodb://localhost/lab-mongoose-recipes', {
+   useNewUrlParser: true,
+   useUnifiedTopology: true,
+   useFindAndModify: false
+})
+   .then(() => {
+       console.log('successfully connected');
+   })
+   .catch(err => {
+       console.log('an error occurred: ', err);
+   })
+
+
+// Recipe.create({
+//     title: 'poires Belle-Hélène',
+//     level: 'Easy Peasy',
+//     cuisine: 'french',
+//     dishType: 'dessert',
+//     duration: 60,
+//     creator: 'Corentine'
+// })
+//     .then(recipe => {
+//         console.log('this recipe was created: ', recipe);
+//     })
+//     .catch(err => {
+//         console.log(err);
+// })
+
+
+const recipes = require('./data.json');
+console.log(recipes)
+
+
